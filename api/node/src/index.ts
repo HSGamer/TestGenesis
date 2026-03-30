93288295400a7cf6f73db715496fe6808a4d5d83
hsgamer <huynhqtienvtag@gmail.com>
2026-03-30 21:18:05 +0700

    protocol implementation

---
+++ api/node/src/index.ts
@@ -0,0 +1,6 @@
+export * from './generated/uap_pb.js';
+export * from './generated/uap_connect.js';
+export * from './generated/job_pb.js';
+export * from './generated/translation_pb.js';
+export * from './generated/common_pb.js';
+export * from './generated/capabilities_pb.js';
